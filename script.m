830c01e096dd57a3d03b3b0f6ef08678352f8f24
Ignacio Vidaurreta <igvidaurreta@gmail.com>
2019-05-06 10:20:26 -0300

    Put arguments in variables

--- script.m
+++ script.m
@@ -1,8 +1,12 @@
 1;# Script file?? Maybe this should be a function file
 
+terrain = './terrains/terrain05.data';
+max_epochs = 5;
+sample_number = 200;
+
 addpath('./activation_derivatives')
 addpath('./activation_functions')
 addpath('./utility_functions')
 addpath('./terrains')
 
-network_setup('./terrains/terrain05.data', 5, 20)
+network_setup(terrain, max_epochs, sample_number)
