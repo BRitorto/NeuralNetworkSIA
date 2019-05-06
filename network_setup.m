a034fc8315eb5d819f69671b37165fdffc834af7
Ignacio Vidaurreta <igvidaurreta@gmail.com>
2019-05-06 17:23:32 -0300

    Normalize input

--- network_setup.m
+++ network_setup.m
@@ -1,8 +1,11 @@
 function out = network_setup(terrain_file, epochs, n_samples, arq)
     W = generate_weights_heEtAl(arq);
     full_patterns = load_file(terrain_file);
-    test_patterns = randomize_patterns(full_patterns, n_samples);
+
+    normalized_patterns = normalize(full_patterns);
+
+    test_patterns = randomize_patterns(normalized_patterns, n_samples);
     g = {{@sigmoid_exp, @dsigmoid_exp}, {@sigmoid_exp, @dsigmoid_exp}, {@(x) x, @(x) 1}};
     %out = incremental_learn(W, test_patterns, g, eta=0.1, epochs, random_pass= true, momentum= 0.9, aep = [], with_error=true);
-    out = learn(W, test_patterns, g, eta = 0.0005, epochs, false, false, momentum = 0.9, aep = [], record_error = true);
+    out = learn(W, test_patterns, g, eta = 0.0005, epochs, false, false, momentum = 0.9, aep = [], record_error = true)
 endfunction
--- utility_functions/getMin.m
+++ utility_functions/getMin.m
@@ -3,7 +3,7 @@ function out = getMin(terrain_matrix)
     current_min=0;
     for i=[1: row_num]
         if(terrain_matrix{i}{1}(1) < current_min)
-            current_min=terrain_matrix{i}{1}(1);
+            current_min = terrain_matrix{i}{1}(1);
         endif
 
         if(terrain_matrix{i}{1}(2) < current_min)
