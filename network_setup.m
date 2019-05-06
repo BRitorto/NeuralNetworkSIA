20de58792700d8b43fc6b3317600583f52c1503c
Ignacio Vidaurreta <igvidaurreta@gmail.com>
2019-05-06 12:23:31 -0300

    Make the arq a parameter of network_setup

--- network_setup.m
+++ network_setup.m
@@ -1,8 +1,8 @@
-function out = network_setup(terrain_file, epochs, n_samples)
-    W = generate_weights_heEtAl([2 3 10 1]);
+function out = network_setup(terrain_file, epochs, n_samples, arq)
+    W = generate_weights_heEtAl(arq);
     full_patterns = load_file(terrain_file);
     test_patterns = randomize_patterns(full_patterns, n_samples);
     g = {{@sigmoid_exp, @dsigmoid_exp}, {@sigmoid_exp, @dsigmoid_exp}, {@(x) x, @(x) 1}};
     %out = incremental_learn(W, test_patterns, g, eta=0.1, epochs, random_pass= true, momentum= 0.9, aep = [], with_error=true);
-    out = learn(W, test_patterns, g, eta = 0.1, epochs, false, false, momentum = 0, aep = [], record_error = true);
+    out = learn(W, test_patterns, g, eta = 0.0005, epochs, false, false, momentum = 0.9, aep = [], record_error = true);
 endfunction
--- script.m
+++ script.m
@@ -1,12 +1,12 @@
 1;# Script file?? Maybe this should be a function file
 
 terrain = './terrains/terrain05.data';
-max_epochs = 5;
-sample_number = 200;
-
+max_epochs = 30;
+sample_number = 400;
+arq = [2 10 10 1];
 addpath('./activation_derivatives')
 addpath('./activation_functions')
 addpath('./utility_functions')
 addpath('./terrains')
 
-network_setup(terrain, max_epochs, sample_number)
+network_setup(terrain, max_epochs, sample_number, arq);
