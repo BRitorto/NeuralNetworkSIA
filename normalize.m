ee17a97676e2fc2658cff449890036b5c27fed43
Ignacio Vidaurreta <igvidaurreta@gmail.com>
2019-05-08 16:08:42 -0300

    Add minMax normalization again

--- normalize.m
+++ normalize.m
@@ -1,25 +1,21 @@
-function out=normalize(patterns)
+function out= normalize(patterns)
+
+    max_value = getMax(patterns)
+    min_value = getMin(patterns)
     row_num = size(patterns, 2);
-    for i=[1:row_num]
-      norma=norm(patterns{i}{1});
-      patterns{i}{1}(1)=patterns{i}{1}(1)/norma;
-      patterns{i}{1}(2)=patterns{i}{1}(2)/norma;
-    endfor
-    out=patterns;
+    for i=[1: row_num]
+        
+        aux_value=patterns{i}{1}(1);
+        patterns{i}{1}(1) = (aux_value - min_value) / (max_value - min_value);
+
+        aux_value = patterns{i}{1}(2);
+        patterns{i}{1}(2) = (aux_value - min_value) / (max_value - min_value);
+        
+
+        aux_value = patterns{i}{2};
+        patterns{i}{2} = (aux_value - min_value) / (max_value - min_value);
+
+        
+    endfor 
+    out = patterns;
 endfunction
-       
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
