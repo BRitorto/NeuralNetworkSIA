d30c7886a7ec6bcff3db590e78cb438adec7f175
Mica Banfi <mbanfi@itba.edu.ar>
2019-05-08 15:37:40 -0300

    chau warning

--- normalize.m
+++ normalize.m
@@ -2,8 +2,10 @@ function out=normalize(patterns)
     row_num = size(patterns, 2);
     for i=[1:row_num]
       norma=norm(patterns{i}{1});
-      patterns{i}{1}(1)=patterns{i}{1}(1)/norma;
-      patterns{i}{1}(2)=patterns{i}{1}(2)/norma;
+      if(norma!=0)
+        patterns{i}{1}(1)=patterns{i}{1}(1)/norma;
+        patterns{i}{1}(2)=patterns{i}{1}(2)/norma;
+      endif
     endfor
     out=patterns;
 endfunction
